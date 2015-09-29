fb6e837413e036aac4a2ae7f83f6867d305f6589
Kevin Ewing <notmyuwmail@gmail.com>
2015-09-28 19:02:44 -0700

    adding bower and gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var glob = require("glob")
+
+
+
+
+
+gulp.task('default', function() {
+    console.log('BuuUUuuUUuurrrp!');
+});
+
+gulp.task('compile_hbs', function() {
+
+});
